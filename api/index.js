29557a42e420811ade301b1732c4081eee284925
Behzadkk <bhzdkkvnd@gmail.com>
2019-05-09 12:05:24 +0100

    users added to database and routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 const filename = "database/database.sqlite";
 const sqlite3 = require("sqlite3").verbose();
@@ -42,6 +43,9 @@ router.get("/projects/title/:title", function(req, res) {
 // POST a new project
 router.post("/projects/", function(req, res) {
   const project = req.body;
+  // add the relation between project and hirer to use in $hirer.id
+  // for now it will use hard coded id = 1
+  // it should check if the user id exist
   const insert = `INSERT INTO projects (title, details, price, deadline, project_length, hirer_id, created_date) VALUES ($title, $details, $price, $deadline, $project_length, $hirer_id, $created_date)`;
   db.run(
     insert,
@@ -51,7 +55,7 @@ router.post("/projects/", function(req, res) {
       $price: project.price,
       $deadline: project.deadline,
       $project_length: project.project_length,
-      $hirer_id: project.hirer_id,
+      $hirer_id: 1,
       $created_date: new Date().toISOString()
     },
     function(err) {
@@ -124,6 +128,111 @@ router.delete("/projects/:id", function(req, res) {
     }
     console.log("Request succeeded, data erased");
     res.sendStatus(200);
+  });
+});
+
+// CREATE a user
+router.post("/users/", function(req, res) {
+  let user = {};
+  bcrypt
+    .hash(req.body.password, 12)
+    .then(hashedPassword => {
+      user.email = req.body.email;
+      user.password = hashedPassword;
+      return user;
+    })
+    .then(user => {
+      // I need check if a user exists
+      return user;
+    })
+    .then(user => {
+      const insert = `INSERT INTO users (email, password) VALUES ($email, $password)`;
+      db.run(
+        insert,
+        {
+          $email: user.email,
+          $password: user.password
+        },
+        function(err) {
+          if (err) {
+            console.log("ERROR fetching from the database:", err);
+            return;
+          }
+          console.log(
+            "Request succeeded, new data inserted, id =",
+            this.lastID
+          );
+        }
+      );
+    })
+    .catch(err => {
+      throw err;
+    });
+  res.sendStatus(200);
+});
+
+// GET all users
+router.get("/users", function(req, res) {
+  const sql = "SELECT * FROM users";
+  db.all(sql, [], (err, rows) => {
+    rows.forEach(user => (user.password = null));
+    if (err) {
+      console.log("ERROR fetching from the database:", err);
+      return;
+    }
+    console.log("Request succeeded, new data fetched", rows);
+    res.status(200).json({
+      users: rows
+    });
+  });
+});
+// UPDATE  a user
+
+// DELETE a user
+
+// a user posts a project
+router.post("/users/:id/projects/", function(req, res) {
+  const hirer_id = req.params.id;
+  const project = req.body;
+
+  // it should check if the user id exist
+  // ??
+  const insert = `INSERT INTO projects (title, details, price, deadline, project_length, hirer_id, created_date) VALUES ($title, $details, $price, $deadline, $project_length, $hirer_id, $created_date)`;
+  db.run(
+    insert,
+    {
+      $title: project.title,
+      $details: project.details,
+      $price: project.price,
+      $deadline: project.deadline,
+      $project_length: project.project_length,
+      $hirer_id: hirer_id,
+      $created_date: new Date().toISOString()
+    },
+    function(err) {
+      if (err) {
+        console.log("ERROR fetching from the database:", err);
+        return;
+      }
+      console.log("Request succeeded, new data inserted, id =", this.lastID);
+    }
+  );
+  res.sendStatus(200);
+});
+
+// Get a user's all projects
+router.get("/users/:id/projects/", function(req, res) {
+  const hirer_id = req.params.id;
+  const sql = "SELECT * FROM projects WHERE hirer_id =?";
+  db.get(sql, [hirer_id], (err, rows) => {
+    if (err) {
+      console.log("ERROR fetching from the database:", err);
+      return;
+    }
+    console.log("Request succeeded, new data fetched", rows);
+    res.status(200).json({
+      projects: rows
+    });
   });
 });
 module.exports = router;
--- database/schema.sql
+++ database/schema.sql
@@ -5,12 +5,33 @@ CREATE TABLE projects (
     price NUMBER,
     deadline TEXT,
     project_length NUMBER,
-    hirer_id NUMBER,
+    hirer_id NUMBER NOT NULL,
     created_date TEXT,
-    updated_date TEXT
+    updated_date TEXT,
+    FOREIGN KEY
+(hirer_id) REFERENCES users
+(id)
 );
 
-INSERT INTO projects
-    (title, details, price)
-VALUES
-    ("test", "check details", 10);
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE applications (
+    application_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY
+(project_id) REFERENCES projects
+(id),
+    FOREIGN KEY
+(user_id) REFERENCES users
+(id)
+);
+
+-- INSERT INTO projects
+--     (title, details, price)
+-- VALUES
+--     ("test", "check details", 10);
