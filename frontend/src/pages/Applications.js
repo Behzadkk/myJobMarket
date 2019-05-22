00517271cd91edc9bd67f2dc74b704bb99040772
Behzadkk <bhzdkkvnd@gmail.com>
2019-05-22 13:17:18 +0100

    apply for a project

--- frontend/src/pages/Applications.js
+++ frontend/src/pages/Applications.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
 
+import Spinner from "../components/Spinner/Spinner";
+
 class ApplicationsPage extends Component {
+  state = {
+    isLoading: false,
+    applications: []
+  };
+
+  componentDidMount() {
+    this.fetchApplications();
+  }
+
+  fetchApplications = () => {
+    this.setState({ isLoading: true });
+    fetch("/api/applications")
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        this.setState({ applications: resData.application, isLoading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
   render() {
-    return <h1>The Applications Page</h1>;
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <ul>
+            {this.state.applications.map(app => (
+              <li key={app.id}>{app.id}</li>
+            ))}
+          </ul>
+        )}
+      </React.Fragment>
+    );
   }
 }
 
